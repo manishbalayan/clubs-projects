8477a292fd8dd4d5d4ca5727bf0364e156b2ac87
Harshit-jain-57 <jainharshit877@gmail.com>
2025-12-01 20:19:45 +0530

    temp:User Controllers

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -3,21 +3,29 @@ import prisma from "../db/prisma.js";
 import bcrypt from "bcrypt";
 
 export const getSingleUser = async (req, res, next) => {
-  const { id } = req.params;
+  const id  = Number(req.params.id);
   try {
     const User = await prisma.user.findUnique({
       where: {
         id: id,
       },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+        createdAt: true,
+        updatedAt: true,
+      },
     });
     res.status(200).send(User);
   } catch (err) {
+    console.error(err)
     next(err);
   }
 };
 export const deleteUser = async (req, res, next) => {
-  const { id } = req.params;
-  // ALSO NEED TO CHECK WHEATHER THE USER IS AUTHORIZED TO DELETE THE USER WITH USERID
+  const id = Number(req.params.id);
   try {
     const user = await prisma.user.findUnique({
       where: {
@@ -36,11 +44,13 @@ export const deleteUser = async (req, res, next) => {
   }
 };
 export const updateUser = async (req, res, next) => {
-  const { id } = req.params;
+  const id = Number(req.params.id);
   const data = req.body;
-  // ALSO NEED TO CHECK WHEATHER THE USER IS AUTHORIZED TO UPDATE THE USER WITH USERID
-  //THIS WE GONNA DO AFTERWARDS BCOZ AUTH NEEDED FOR PASSWORD
+  if (Object.keys(data).length === 0) {
+    return next(createError(400, "No data provided for update"));
+  }
   try {
+  
     const user = await prisma.user.findUnique({
       where: {
         id: id,
@@ -70,6 +80,9 @@ export const updateUser = async (req, res, next) => {
     });
     res.status(200).send(updatedUser);
   } catch (err) {
+    if (err.code === 'P2002') { // Prisma unique constraint error
+      return next(createError(400, "Email already exists"));
+    }
     next(err);
   }
 };
